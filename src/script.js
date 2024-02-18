5a86039027f6ad58ffdf792aeea3e121b7322e57
Svetlana <art.soul.space@yandex.ru>
2024-02-18 12:27:38 +0300

    added first game

--- src/script.js
+++ src/script.js
@@ -5,14 +5,19 @@ function seasons() {
 
   if (month === 12 || month === 1 || month === 2) {
     console.log("Зима");
+    alert("Зима")
   } else if (month === 3 || month === 4 || month === 5) {
     console.log("Весна");
+    alert("Весна")
   } else if (month === 6 || month === 7 || month === 8) {
     console.log("Лето");
+    alert("Лето")
   } else if (month === 9 || month === 10 || month === 11) {
     console.log("Осень");
+    alert("Осень")
   } else {
     console.log("Error");
+    alert("Error")
   }
 }
 
